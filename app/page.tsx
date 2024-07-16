f60e2bcfaa78fcb969637cc1189a4272c2156cba
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2024-07-16 15:59:09 +0300

    remove vercel ad

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
+import DeployButton from '../components/DeployButton';
+import AuthButton from '../components/AuthButton';
+import { createClient } from '@/utils/supabase/server';
+import ConnectSupabaseSteps from '@/components/tutorial/ConnectSupabaseSteps';
+import SignUpUserSteps from '@/components/tutorial/SignUpUserSteps';
+import Header from '@/components/Header';
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -23,7 +23,6 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
@@ -38,7 +37,7 @@ export default async function Index() {
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{" "}
+          Powered by{' '}
           <a
             href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
             target="_blank"
