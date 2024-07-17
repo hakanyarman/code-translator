6aa2d6fa3051e3d06835b2731099eadda205a08d
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2024-07-17 11:20:45 +0300

    design the translate page roughly

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,6 +1,6 @@
-import { createClient } from "@/utils/supabase/server";
-import Link from "next/link";
-import { redirect } from "next/navigation";
+import { createClient } from '@/utils/supabase/server';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -10,18 +10,18 @@ export default async function AuthButton() {
   } = await supabase.auth.getUser();
 
   const signOut = async () => {
-    "use server";
+    'use server';
 
     const supabase = createClient();
     await supabase.auth.signOut();
-    return redirect("/login");
+    return redirect('/login');
   };
 
   return user ? (
     <div className="flex items-center gap-4">
       Hey, {user.email}!
       <form action={signOut}>
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
           Logout
         </button>
       </form>
@@ -29,7 +29,7 @@ export default async function AuthButton() {
   ) : (
     <Link
       href="/login"
-      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
     >
       Login
     </Link>
--- components/Header.tsx
+++ components/Header.tsx
@@ -16,12 +16,32 @@ export default function Header() {
         <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
           <NextLogo />
         </a> */}
-        <h2>Code Translator App</h2>
+        <h2 className="text-blue-600 text-xl">Code Translator App</h2>
       </div>
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        {/* The fastest way to build apps with{" "} */}
-      </p>
+      <div className="shadow-md shadow-indigo-500/40">
+        <p className="text-3xl lg:text-xl !leading-tight mx-auto max-w-xl text-center ">
+          {/* The fastest way to build apps with{" "} */}
+          Easily and reliably convert your code from one programming language to
+          another! Our application supports code translations between{' '}
+          <a className="underline decoration-pink-500 decoration-dashed decoration-2">
+            Python
+          </a>{' '}
+          ,
+          <a className="underline decoration-sky-500 decoration-dashed decoration-2">
+            Javascript
+          </a>
+          ,{' '}
+          <a className="underline decoration-indigo-500 decoration-dashed decoration-2">
+            Java
+          </a>
+          ,{' '}
+          <a className="underline decoration-orange-500 decoration-dashed decoration-2">
+            C#
+          </a>
+          , and many more.
+        </p>
+      </div>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
   );
