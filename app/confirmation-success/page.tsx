8863ab1d710bcb891ba9bf9d44cb8fadae44b6e6
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2024-07-29 13:28:35 +0300

    fix deployment issue

--- app/confirmation-success/page.tsx
+++ app/confirmation-success/page.tsx
@@ -1,16 +1,13 @@
+// app/verification-success/page.tsx
 import React from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
 
-export default function ConfirmationSuccess() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get('token'); // Eğer URL'de `token` parametresi varsa bu şekilde alınabilir
-
+const VerificationSuccess = () => {
   return (
-    <div className="container mx-auto text-center p-4">
-      <h1 className="text-2xl font-bold">Üyeliğiniz Doğrulandı!</h1>
-      <p className="mt-4">
-        Hesabınız başarıyla doğrulandı. Şimdi giriş yapabilirsiniz.
-      </p>
+    <div>
+      <h1>Üyeliğiniz Onaylanmıştır!</h1>
+      <p>Artık uygulamamıza giriş yapabilirsiniz.</p>
     </div>
   );
-}
+};
+
+export default VerificationSuccess;
